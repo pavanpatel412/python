6335c15b8d75a51d3d9a87b4b7681b84695f501d
pavanpatel412 <144308778+pavanpatel412@users.noreply.github.com>
2023-09-20 16:51:40 +0530

    Add files via upload

---
+++ file handling.py
@@ -0,0 +1,5 @@
+import os 
+ os.mkdir("C:\\rajeshkumar\\lokesh.txt")
+print("the current directory is:",os.listdir())
+os.removedirs("c:\\rajeshkumar\\lokesh.txt")
+print("the current directory is:",os.listdir())
