79b2e22a486be1d80a2ffc17dd6eaef3e6207afe
pavanpatel412 <144308778+pavanpatel412@users.noreply.github.com>
2023-11-20 22:41:50 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,9 @@
+myname_str = "my name is pavan"
+print("the type of myname_str is :",type(myname_str))
+print("index  3 in myname_str is:",myname_str[3])
+print("slicing between the 1 to 6 is ;",myname_str[0:7])
+print("reversing the string is:",myname_str[::-1])
+print("slicing between reverse:",myname_str[-1:8])
+print("reverse the string without using two duble:",myname_str[-1:-19])#it can't work.....
+print("reversing the string between three steps:",myname_str[-1::-3]) #reverse slicing between three steps:
+print("forward slicing between two steps:",myname_str[-1::2])      #it prints the last letter only.       
